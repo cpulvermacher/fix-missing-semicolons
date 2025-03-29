5ac1bcb424444026178e3fbb21ddbe103b260b61
Christian Pulvermacher <christian.pulvermacher@posteo.de>
2025-03-29 20:30:15 +0900

    trace log level for tests (disabled)

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -13,7 +13,11 @@ async function main() {
         const extensionTestsPath = path.resolve(__dirname, './vscode/index');
 
         // Download VS Code, unzip it and run the integration test
-        await runTests({ extensionDevelopmentPath, extensionTestsPath });
+        await runTests({
+            extensionDevelopmentPath,
+            extensionTestsPath,
+            // launchArgs: ['--log=trace'],
+        });
     } catch (err) {
         console.error('Failed to run tests', err);
         process.exit(1);
